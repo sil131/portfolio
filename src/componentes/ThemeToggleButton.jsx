d2e4ff06320a98516bbd41cb132741f2662d44b5
sil131 <129982739+sil131@users.noreply.github.com>
2023-07-18 20:49:54 +0200

    Add files via upload

--- src/componentes/ThemeToggleButton.jsx
+++ src/componentes/ThemeToggleButton.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
+import sunIcon from '../assets/sun.svg';
+import moonIcon from '../assets/moon.svg'
 
-const ThemeToggleButton = () => {
+const ThemeToggleButton = ({burguerFunction}) => {
   const [isDarkMode, setIsDarkMode] = useState(false);
 
   const handleToggleTheme = () => {
@@ -13,12 +15,38 @@ const ThemeToggleButton = () => {
   };
 
   return (
+    <>
     <button
       onClick={handleToggleTheme}
       className={`theme-toggle-button ${isDarkMode ? 'dark-mode' : 'light-mode'}`}
     >
-      {isDarkMode ? 'Light Mode' : 'Dark Mode'}
+      {isDarkMode ? (
+          <img src={sunIcon} alt="Sun Icon" style={{ height: '24px', width: '24px' }} />
+        ) : (
+          <img src={moonIcon} alt="Moon Icon" style={{ height: '24px', width: '24px' }} />
+        )}
+      
+
+      
     </button>
+    
+    <button className="burger-menu" id="burger-menu" onClick={burguerFunction}>
+        
+    <svg
+    className='section'
+    fill={isDarkMode ? '#ffffff' : '#333333'}
+    height="32px"
+    id="Layer_1"
+    style={{ enableBackground: 'new 0 0 32 32' }}
+    version="1.1"
+    viewBox="0 0 32 32"
+    width="32px"
+  >
+    <path d="M4,10h24c1.104,0,2-0.896,2-2s-0.896-2-2-2H4C2.896,6,2,6.896,2,8S2.896,10,4,10z M28,14H4c-1.104,0-2,0.896-2,2  s0.896,2,2,2h24c1.104,0,2-0.896,2-2S29.104,14,28,14z M28,22H4c-1.104,0-2,0.896-2,2s0.896,2,2,2h24c1.104,0,2-0.896,2-2  S29.104,22,28,22z" />
+  </svg>
+        </button>
+    
+    </>
   );
 };
 
--- src/componentes/menu.jsx
+++ src/componentes/menu.jsx
@@ -1,21 +1,25 @@
 import ThemeToggleButton from "./ThemeToggleButton";
 import Switch from "./ThemeToggleButton";
+import hamburguer from '../assets/hamburguesa.svg'
 
 function Menu (){
+
+  function open(){
+    const navigation = document.getElementById('navigation');
+    navigation.classList.toggle('ocultar');
+  }
     return(
         <>
-        <ul className="navigation">
+        <ul className="navigation ocultar" id="navigation">
           <li><a href="#about" className="nav-link">About</a></li>
           <li><a href="#skills" className="nav-link">Skills</a></li>
           <li><a href="#projects" className="nav-link">Projects</a></li>
           <li><a href="#contact" className="nav-link">Contact</a></li> 
-          <ThemeToggleButton />
         </ul>
        
-        <button className="burger-menu" id="burger-menu">
-          <ion-icon className="bars" name="menu-outline"></ion-icon>
-        
-        </button>
+       <div className="navigation-buttons">
+          <ThemeToggleButton burguerFunction={open}/>
+       </div>
 
         
         </>
