bde80cf1c7f2a7c16e2f5749c8a0e23d184c57bf
sil131 <sil131@gmail.com>
2023-07-25 22:59:30 +0200

    camnio1

--- src/componentes/ThemeToggleButton.jsx
+++ src/componentes/ThemeToggleButton.jsx
@@ -14,24 +14,32 @@ const ThemeToggleButton = ({ burguerFunction }) => {
     }
   };
 
- 
-  const iconBorderStyle = { stroke: '#4756df' };
-
   return (
     <>
-      <button className="burger-menu" id="burger-menu" onClick={burguerFunction}>
-        {/* ... */}
-      </button>
-      
       <button
         onClick={handleToggleTheme}
         className={`theme-toggle-button ${isDarkMode ? 'dark-mode' : 'light-mode'}`}
       >
         {!isDarkMode ? (
-          <img src={sunIcon} alt="Sun Icon" style={{ height: '24px', width: '24px', fill: '#ffffff', ...iconBorderStyle }} />
+          <img src={moonIcon} alt="Moon Icon" style={{ height: '24px', width: '24px', fill: '#4756df' }} />
         ) : (
-          <img src={moonIcon} alt="Moon Icon" style={{ height: '24px', width: '24px', fill: '#ffffff', ...iconBorderStyle }} />
+          <img src={sunIcon} alt="Sun Icon" style={{ height: '24px', width: '24px', fill: '#4756df' }} />
         )}
+      </button>
+
+      <button className="burger-menu" id="burger-menu" onClick={burguerFunction}>
+        <svg
+          className='section'
+          fill={isDarkMode ? '#ffffff' : '#333333'}
+          height="32px"
+          id="Layer_1"
+          style={{ enableBackground: 'new 0 0 32 32' }}
+          version="1.1"
+          viewBox="0 0 32 32"
+          width="32px"
+        >
+          <path d="M4,10h24c1.104,0,2-0.896,2-2s-0.896-2-2-2H4C2.896,6,2,6.896,2,8S2.896,10,4,10z M28,14H4c-1.104,0-2,0.896-2,2  s0.896,2,2,2h24c1.104,0,2-0.896,2-2S29.104,14,28,14z M28,22H4c-1.104,0-2,0.896-2,2s0.896,2,2,2h24c1.104,0,2-0.896,2-2  S29.104,22,28,22z" />
+        </svg>
       </button>
     </>
   );
