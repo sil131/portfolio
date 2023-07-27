e82ccb5616f5d8ddccb455aae7495126deccbbd6
sil131 <sil131@gmail.com>
2023-07-27 18:08:22 +0200

    commit10

--- src/componentes/socials.jsx
+++ src/componentes/socials.jsx
@@ -1,8 +1,5 @@
 import React, { useState } from 'react';
-import twitter from '../assets/icons8-twitter-circled.gif';
-import instagram from '../assets/icons8-instagram.gif';
-import linkedin from '../assets/icons8-linkedin-circled.gif';
-import github from '../assets/icons8-github.gif';
+import 'boxicons'
 
 function Socials() {
   const [isDarkMode, setIsDarkMode] = useState(false);
@@ -15,16 +12,16 @@ function Socials() {
   return (
     <div className={`socials ${isDarkMode ? 'dark-mode' : 'light-mode'}`}>
       <a href="#" target="_blank">
-        <img src={twitter} alt="Twitter" loading="lazy" className="socicon" />
+      <box-icon type='logo' name='twitter'animation='tada'></box-icon> 
       </a>
       <a href="#" target="_blank">
-        <img src={instagram} alt="Instagram" loading="lazy" className="socicon" />
+      <box-icon type='logo' name='instagram-alt'animation='tada'></box-icon>
       </a>
       <a href="#" target="_blank">
-        <img src={linkedin} alt="Linkedin" loading="lazy" className="socicon" />
+      <box-icon type='logo' name='linkedin-square'animation='tada'></box-icon>
       </a>
       <a href="#" target="_blank">
-        <img src={github} alt="Github" className="socicon" />
+      <box-icon type='logo' name='github'animation='tada'></box-icon>
       </a>
     </div>
   );
